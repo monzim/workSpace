732906e40056263c7a6035c73023939754e2e828
Azraf Al Monzim <dev@monzim.com>
2023-08-14 16:35:44 +0600

    LOL

--- script/start/setup.sh
+++ script/start/setup.sh
@@ -122,6 +122,6 @@ cd script/AnsibleDevSetup/playbooks
 pwd
 print_message $YELLOW "$TASK" "Running Ansible Playbook..."
 
-sudo ansible-playbook -i hosts setup_dev_environment.yml -k
+sudo ansible-playbook -v -i hosts setup_dev_environment.yml -k
 
 print_message $GREEN "$SUCCESS" "Ansible Playbook completed successfully!"
